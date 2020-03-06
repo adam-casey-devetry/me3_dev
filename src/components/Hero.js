2147db9e8cc38e38df38cda990dff40266d036f9
Adam Casey <adam.casey@devetry.com>
2020-03-06 10:18:20 -0700

    Create IframePage component for embedded video

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,17 @@
 import React from "react";
+import { MDBContainer, MDBIframe } from "mdbreact";
 
-export default function Hero() {
+const IframePage = () => {
+  return (
+    <MDBContainer className="text-center">
+      <MDBIframe src="https://player.vimeo.com/video/268111200?autoplay=1&amp;loop=1&amp;autopause=0&amp;background=1" />
+    </MDBContainer>
+  );
+};
+
+export default IframePage;
+
+/* export default function Hero() {
   return (
     <section className="hero is-primary">
       <div className="hero-body">
@@ -11,3 +22,18 @@ export default function Hero() {
     </section>
   );
 }
+<div className="video-responsive">
+<div style={{ padding: "39.5 0 0 0", position: "relative" }}></div>
+</div>
+
+<Iframe
+src="https://player.vimeo.com/video/268111200?autoplay=1&amp;loop=1&amp;autopause=0&amp;background=1"
+style={{
+  position: "relative",
+  width: "5000px",
+  height: "1000px"
+}}
+title="Visit ASU"
+frameBorder="0"
+allowFullScreen="true"
+></Iframe> */
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
-import Hero from "./Hero";
+import IframePage from "./Hero";
 
 export default function Home() {
   return (
     <Fragment>
-      <Hero />
       <div className="box cta">
+        <IframePage />
         <p className="has-text-centered"></p>
       </div>
     </Fragment>
