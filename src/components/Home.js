2983d1355db17f91d6f6064204d6845081eb9a07
Adam Casey <adam.casey@devetry.com>
2020-03-09 13:04:29 -0600

    Configure GET request but receive CORS errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
 import React, { Fragment } from "react";
 import PhotoGrid from "./PhotoGrid";
+import { API } from "aws-amplify";
 import "../index.css";
 
 export default function Home(props) {
   if (props.auth.isAuthenticated) {
     console.log("User is authenticated");
+    let apiName = "adamTestAPI_West";
+    let path = "/user/2";
+    let myInit = {
+      // OPTIONAL
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET"
+      }, // OPTIONAL
+      response: true, // OPTIONAL (return the entire Axios response object instead of only response.data)
+      queryStringParameters: {
+        // OPTIONAL
+        name: "param"
+      }
+    };
+    API.get(apiName, path, myInit)
+      .then(response => {
+        console.log("GET response: " + JSON.stringify(response));
+      })
+      .catch(error => {
+        console.log("Error: " + error);
+      });
   } else {
     console.log("User is NOT authenticated");
   }
