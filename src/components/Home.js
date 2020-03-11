d68d74a938b34d0b3992b660f77d0cd672589794
Adam Casey <adam.casey@devetry.com>
2020-03-11 13:06:24 -0600

    Add useEffect() hook but rerender Home component twice

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,50 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import PhotoGrid from "./PhotoGrid";
 
-import { API } from "aws-amplify";
+import { API, Auth } from "aws-amplify";
 
 import "../index.css";
 
 export default function Home(props) {
-  if (props.auth.isAuthenticated) {
-    console.log("User is authenticated");
-    let apiName = "adamTestAPI_West";
-    let path = "/user/1";
-    let myInit = {
-      // OPTIONAL
+  const [jwToken, setJWToken] = useState();
+
+  async function getData() {
+    let idToken = {
       headers: {
-        "Content-Type": "application/json"
-      }, // OPTIONAL
-      response: true // OPTIONAL (return the entire Axios response object instead of only response.data)
+        Authorization: `Bearer ${(await Auth.currentSession())
+          .getIdToken()
+          .getJwtToken()}`
+      }
     };
-    API.get(apiName, path, myInit)
-      .then(response => {
-        console.log("GET response: " + JSON.stringify(response));
-      })
-      .catch(error => {
-        console.log("Error: " + error);
-      });
-  } else {
-    console.log("User is NOT authenticated");
+    setJWToken(idToken.headers.Authorization);
+    //return idToken;
   }
+
+  useEffect(() => {
+    if (props.auth.isAuthenticated) {
+      console.log("User is authenticated");
+      getData().then(console.log("jwToken: " + jwToken));
+      let apiName = "adamTestAPI_West";
+      let path = "/user/1";
+      let myInit = {
+        // OPTIONAL
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: jwToken
+        }, // OPTIONAL
+        response: true // OPTIONAL (return the entire Axios response object instead of only response.data)
+      };
+      API.get(apiName, path, myInit)
+        .then(response => {
+          console.log("GET response: " + JSON.stringify(response.data));
+        })
+        .catch(error => {
+          console.log("Error: " + error);
+        });
+    } else {
+      console.log("User is NOT authenticated");
+    }
+  }, [jwToken]);
 
   return (
     <Fragment>
